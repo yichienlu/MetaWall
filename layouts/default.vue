16ded670656b89b0fd8c22013cbe7e4e22d9c6c9
yichienlu <yichien.lu89@gmail.com>
2024-04-25 14:02:08 +0800

    fix: header, menu sticky

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,17 @@
 
 <template>
   <div class="bg-grid min-h-dvh relative pb-24">
-    <Header />
+    <Header class="sticky top-0 z-20" />
+    
     <div class="px-4 pt-12">
       <div class="container grid md:grid-cols-3 gap-4">
         <div  class="col-span-2">
         <slot />
 
         </div>
-        <Menu class="fixed bottom-4 left-0 md:relative md:bottom-auto" />
+        <div class="relative">
+          <Menu class="fixed bottom-4 left-0 md:sticky md:top-[104px] md:bottom-auto" />
+        </div>
       </div>
     </div>
 
